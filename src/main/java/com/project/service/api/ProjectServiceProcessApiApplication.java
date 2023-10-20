760d0156f70cd022a0bc685f13b0ec256c0c93ac
qwer9052 <qwer9052@gmail.com>
2023-10-20 19:33:35 +0900

    init

---
+++ src/main/java/com/project/service/api/ProjectServiceProcessApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.service.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectServiceProcessApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectServiceProcessApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/service/api/ProjectServiceProcessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.service.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectServiceProcessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
